94ead24fc3baae53d2c65a452cf0e30bc76b8cf7
Oleg Mitrakhovich <olegmit9@hotmail.com>
2020-08-17 13:53:32 -0400

    added eth.subscribe to blocks

--- profitable-flashloans/9-setup-project/run-arbitrage.js
+++ profitable-flashloans/9-setup-project/run-arbitrage.js
@@ -3,3 +3,11 @@ const Web3 = require('web3');
 const web3 = new Web3(
          new Web3.providers.WebsocketProvider(process.env.INFURA_URL)
 );
+
+web3.eth.subscribe('newBlockHeaders')
+    .on('data', async block => {
+        console.log(`New block received. Block # ${block.number}`);
+    })
+    .on('error', error => {
+        console.log(error);
+    });
